5662b2b481220d2b9d03d9ed400f1a7875ff0e01
maya88en <maya88en@gmail.com>
2020-03-11 16:29:56 +0200

    init

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :invoices
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'invoices#index'
+  root 'welcome#index'
 end
