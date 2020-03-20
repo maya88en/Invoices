dfa710eba5b503cfac69f1fae99611826ca48164
maya88en <maya88en@gmail.com>
2020-03-20 16:05:08 +0200

    add features

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,8 +9,10 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
+
 //= require rails-ujs
 //= require bootstrap-sprockets
-//= require datepicker
 //= require_tree .
+//= require main
+
--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,11 @@
+//= require jquery
+
 $(document).ready(function(){
 
-	$('.datepicker').datepicker({
-		format: 'yyyy-mm-dd'
-    });
+	$('#addrow').click(function(){
+		$('.item-row:last').after('<tr class="item-row"><td><input class="form-control item_name" placeholder="Item name" /></td><td><input class="form-control item_desc" placeholder="Item description" /></td><td><input class="form-control cost" value="0.00" /></td><td><input class="form-control qty" value="0" /></td><td class="price_td"><span class="price">0.00</span><span class="subtotal_currency"></span></td><td class="delete_td"><a class="delete" href="javascript:;" title="Remove row"><span class="ti-close"></span></a></td></tr>');
+		if ($('.delete').length > 0) $('.delete').show();
+		bind1();
+	});
+	
 });
